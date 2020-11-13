95786d0d1f13e3a7ced700de55b9c5f79ed6c08a
VincentSastra <vincent.sastra@gmail.com>
2020-11-13 00:12:33 -0800

    Made the Public area create table script and deletion

--- bin/ca/ubc/cs304/sql/scripts/databaseSetup.sql
+++ bin/ca/ubc/cs304/sql/scripts/databaseSetup.sql
@@ -3,3 +3,12 @@ CREATE TABLE Branch (
 	branch_name varchar2(50) not null,
 	branch_address varchar2(100) not null
 );
+
+create table "PublicArea"
+(
+    AREA_ID         NUMBER   not null
+        constraint PUBLICAREA_PK
+            primary key,
+    AREA_NAME       CHAR(50) not null,
+    AREA_IS_OUTDOOR CHAR     not null
+)
--- src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
+++ src/ca/ubc/cs304/database/DatabaseConnectionHandler.java
@@ -196,6 +196,7 @@ public class DatabaseConnectionHandler {
 			while(rs.next()) {
 				if(rs.getString(1).toLowerCase().equals("branch")) {
 					stmt.execute("DROP TABLE Branch");
+					stmt.execute("DROP TABLE PublicArea");
 					break;
 				}
 			}
