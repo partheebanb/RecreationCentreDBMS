4cd08aa6c99dbe1b60b50f4dd2c70b3ea028d611
Partheeban Bharati <b.partheeban@hotmail.com>
2020-11-22 00:44:32 -0800

    Added a query to find all the members who have signed up in all the branches

--- src/ca/ubc/cs304/database/MemberHandler.java
+++ src/ca/ubc/cs304/database/MemberHandler.java
@@ -1,9 +1,10 @@
 package ca.ubc.cs304.database;
 
+import ca.ubc.cs304.model.BranchModel;
 import ca.ubc.cs304.model.MemberModel;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 public class MemberHandler {
     private static final String EXCEPTION_TAG = "[EXCEPTION]";
@@ -41,6 +42,36 @@ public class MemberHandler {
         } catch (SQLException e) {
             System.out.println(EXCEPTION_TAG + " " + e.getMessage());
         }
+    }
+
+//    division
+    public MemberModel[] membersInAllBranches() {
+        ArrayList<MemberModel> result = new ArrayList<>();
+
+        try {
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT name FROM member m WHERE NOT EXISTS (SELECT b.branch_id FROM branch b WHERE NOT EXISTS ( SELECT r.branch_id FROM registersIN r WHERE r.branch_id = b.branch_id AND m.member_id = r.member_id");
+
+            while(rs.next()) {
+                MemberModel model = new MemberModel(rs.getInt("member_id"),
+                        rs.getDate("member_since"),
+                        rs.getDate("dob"),
+                        rs.getString("membership_type"),
+                        rs.getString("gender").charAt(0),
+                        rs.getString("first_name"),
+                        rs.getString("last_name"),
+                        rs.getString("email"),
+                        rs.getInt("membership_fee"));
+                result.add(model);
+            }
+
+            rs.close();
+            stmt.close();
+        } catch (SQLException e) {
+            System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+        }
+
+        return result.toArray(new MemberModel[result.size()]);
     }
 }
 
