2872ce47559c617e2ac76c7b2e0149a0f9a821a6
Partheeban Bharati <b.partheeban@hotmail.com>
2020-11-22 17:49:32 -0800

    "Added query to select members in given program"

--- src/ca/ubc/cs304/model/PublicAreaModel.java
+++ src/ca/ubc/cs304/model/PublicAreaModel.java
@@ -6,12 +6,14 @@ public class PublicAreaModel {
     private final String name;
     private final String type;
     private final boolean isOutdoor;
+    private final int branchId;
 
-    public PublicAreaModel(int areaId, String name, String type, boolean isOutdoor) {
+    public PublicAreaModel(int areaId, String name, String type, boolean isOutdoor, int branchId) {
         this.areaId = areaId;
         this.name = name;
         this.type = type;
         this.isOutdoor = isOutdoor;
+        this.branchId = branchId;
     }
 
     public int getAreaId() {
@@ -29,5 +31,7 @@ public class PublicAreaModel {
     public boolean isOutdoor() {
         return isOutdoor;
     }
+
+    public int getBranchId() {return branchId; };
 
 }
